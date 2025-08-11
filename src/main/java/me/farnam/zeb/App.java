6de855f916bfd9b5709ecfaa430221d9e299f919
ferigeek <150931210+ferigeek@users.noreply.github.com>
2025-08-11 22:33:06 +0330

    Edited layout.

--- src/main/java/me/farnam/zeb/App.java
+++ src/main/java/me/farnam/zeb/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
-        scene = new Scene(fxmlLoader.load(), 600, 400);
+        scene = new Scene(fxmlLoader.load(), 600, 475);
         stage.setScene(scene);
         stage.setTitle("Zip / Encrypt / Backup");
         stage.show();
--- src/main/java/me/farnam/zeb/controller/MainController.java
+++ src/main/java/me/farnam/zeb/controller/MainController.java
@@ -3,7 +3,9 @@ package me.farnam.zeb.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
 import me.farnam.zeb.App;
 
 import java.io.IOException;
@@ -15,8 +17,13 @@ public class MainController {
     private Tab gitTab;
     @FXML
     private Tab backupTab;
+    @FXML
+    private Button goBtn;
+    @FXML
+    private TextArea logConsoleTextArea;
 
     public void initialize() throws IOException {
+        logConsoleTextArea.appendText("Welcome to ZEB \uD83D\uDD10!\nLog:\n");
         dirPassChooseTab.setContent(loadFXML("tabs/dir-pass-choose.fxml"));
     }
 
