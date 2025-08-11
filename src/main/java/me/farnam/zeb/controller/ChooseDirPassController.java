19c9e87d53dea3a5ebeed92977a1061f446d6421
ferigeek <150931210+ferigeek@users.noreply.github.com>
2025-08-11 22:13:13 +0330

    Added directory choosing dialog for "choose" button.

--- src/main/java/me/farnam/zeb/controller/ChooseDirPassController.java
+++ src/main/java/me/farnam/zeb/controller/ChooseDirPassController.java
@@ -3,6 +3,9 @@ package me.farnam.zeb.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class ChooseDirPassController {
     @FXML
@@ -20,6 +23,13 @@ public class ChooseDirPassController {
     @FXML
     private PasswordField repeatPassPF;
 
+    /**
+     * If "Use password" checkbox is selected, related password fields are enabled.
+     * <br/>
+     * Otherwise, on deselecting the checkbox, related password fields are disabled.
+     * @param event
+     * ActionEvent
+     */
     @FXML
     private void onPassCheck(ActionEvent event) {
         if (passwordCheck.isSelected()) {
@@ -27,6 +37,29 @@ public class ChooseDirPassController {
         } else {
             disablePasswordFields(true);
         }
+    }
+
+    /**
+     * Directory choosing dialog is shown to user after hitting "choose" button.
+     * The Chosen directory will be shown in the directory path text field.
+     * @param event
+     * ActionEvent
+     */
+    @FXML
+    private void onDirChooseBtn(ActionEvent event) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        directoryChooser.setTitle("Choose directory...");
+        File chosenPath = directoryChooser.showDialog(chooseBtn.getScene().getWindow());
+
+        if (chosenPath == null || !chosenPath.exists()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please choose a valid directory!");
+            alert.show();
+            return;
+        }
+
+        dirPathTF.setText(chosenPath.getAbsolutePath());
     }
 
     private void disablePasswordFields(boolean tf) {
